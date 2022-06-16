489011f624ab2c211f3bb4cc142356bcc26a86cf
anselmeatchogou <anselmeatchogou@gmail.com>
2022-06-16 13:27:53 +0100

    db config

--- connectDB.php
+++ connectDB.php
@@ -1,9 +1,9 @@
 <?php
 /* Database connection settings */
-	$servername = "localhost";
-    $username = "root";		//put your phpmyadmin username.(default is "root")
-    $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "biometricattendace2";
+    $servername = "mysql-ansoatc.alwaysdata.net";
+    $username = "ansoatc";		//put your phpmyadmin username.(default is "root")
+    $password = "anso20092001";			//if your phpmyadmin has a password put it here.(default is "root")
+    $dbname = "ansoatc_biometric_attendance_system_ayeltech";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
